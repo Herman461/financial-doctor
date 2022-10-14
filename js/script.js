d8de96fd1577ff92e4cea64b39fd229d227912f4
Herman461 <zhukovskiyherman@gmail.com>
2022-10-14 07:30:56 +0300

    update landing page

--- js/script.js
+++ js/script.js
@@ -46,74 +46,177 @@ if (sliders) {
 
 function sliders_build_callback() {}
 
-var headerBurger = document.querySelector(".header__burger");
-var header = document.querySelector('.header');
-var timeout = false;
-
-if (headerBurger) {
-  var menuContent = document.querySelector(".menu");
-  headerBurger.addEventListener('click', function () {
-    if (timeout) return;
-    timeout = true;
-
-    if (!headerBurger.classList.contains('active')) {
-      document.body.style.paddingRight = window.innerWidth - document.body.clientWidth + 'px';
-    } else {
-      document.body.style.paddingRight = '0px';
-    }
-
-    document.body.classList.toggle("lock");
-    headerBurger.classList.toggle("active");
-    header.classList.toggle('active');
-    menuContent.classList.toggle("active");
-    setTimeout(function () {
-      timeout = false;
-    }, 300);
-  });
+function DynamicAdapt(type) {
+  this.type = type;
 }
 
-var toTopButton = document.querySelector('.footer__to-top');
+DynamicAdapt.prototype.init = function () {
+  var _this2 = this;
 
-if (toTopButton) {
-  toTopButton.addEventListener('click', function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
+  var _this = this; // массив объектов
+
+
+  this.оbjects = [];
+  this.daClassname = "_dynamic_adapt_"; // массив DOM-элементов
+
+  this.nodes = document.querySelectorAll("[data-da]"); // наполнение оbjects объктами
+
+  for (var i = 0; i < this.nodes.length; i++) {
+    var node = this.nodes[i];
+    var data = node.dataset.da.trim();
+    var dataArray = data.split(",");
+    var оbject = {};
+    оbject.element = node;
+    оbject.parent = node.parentNode;
+    оbject.destination = document.querySelector(dataArray[0].trim());
+    оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+    оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+    оbject.index = this.indexInParent(оbject.parent, оbject.element);
+    this.оbjects.push(оbject);
+  }
+
+  this.arraySort(this.оbjects); // массив уникальных медиа-запросов
+
+  this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+    return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+  }, this);
+  this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+    return Array.prototype.indexOf.call(self, item) === index;
+  }); // навешивание слушателя на медиа-запрос
+  // и вызов обработчика при первом запуске
+
+  var _loop = function _loop(_i) {
+    var media = _this2.mediaQueries[_i];
+    var mediaSplit = String.prototype.split.call(media, ',');
+    var matchMedia = window.matchMedia(mediaSplit[0]);
+    var mediaBreakpoint = mediaSplit[1]; // массив объектов с подходящим брейкпоинтом
+
+    var оbjectsFilter = Array.prototype.filter.call(_this2.оbjects, function (item) {
+      return item.breakpoint === mediaBreakpoint;
     });
-  });
-}
+    matchMedia.addListener(function () {
+      _this.mediaHandler(matchMedia, оbjectsFilter);
+    });
 
-if (document.querySelector('.games__items')) {
-  var gamesSlider = new Swiper('.games__items', {
-    speed: 800,
-    loop: true,
-    spaceBetween: 65,
-    breakpoints: {
-      992: {
-        spaceBetween: 10,
-        slidesPerView: 3
-      },
-      670: {
-        slidesPerView: 2
-      }
-    } // navigation: {
-    //     nextEl: item.parentElement.querySelector(".button-next"),
-    //     prevEl: item.parentElement.querySelector(".button-prev")
-    // },
+    _this2.mediaHandler(matchMedia, оbjectsFilter);
+  };
 
-  });
-}
-
-var puzzleLinks = document.querySelectorAll('[data-puzzle]');
-
-var _loop = function _loop(_index2) {
-  var link = puzzleLinks[_index2];
-  link.addEventListener('click', function (e) {
-    var puzzleName = link.dataset.puzzle;
-    sessionStorage.setItem('puzzleName', puzzleName);
-  });
+  for (var _i = 0; _i < this.mediaQueries.length; _i++) {
+    _loop(_i);
+  }
 };
 
-for (var _index2 = 0; _index2 < puzzleLinks.length; _index2++) {
-  _loop(_index2);
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+  if (matchMedia.matches) {
+    for (var i = 0; i < оbjects.length; i++) {
+      var оbject = оbjects[i];
+      оbject.index = this.indexInParent(оbject.parent, оbject.element);
+      this.moveTo(оbject.place, оbject.element, оbject.destination);
+    }
+  } else {
+    for (var _i2 = 0; _i2 < оbjects.length; _i2++) {
+      var _оbject = оbjects[_i2];
+
+      if (_оbject.element.classList.contains(this.daClassname)) {
+        this.moveBack(_оbject.parent, _оbject.element, _оbject.index);
+      }
+    }
+  }
+}; // Функция перемещения
+
+
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+  element.classList.add(this.daClassname);
+
+  if (place === 'last' || place >= destination.children.length) {
+    destination.insertAdjacentElement('beforeend', element);
+    return;
+  }
+
+  if (place === 'first') {
+    destination.insertAdjacentElement('afterbegin', element);
+    return;
+  }
+
+  destination.children[place].insertAdjacentElement('beforebegin', element);
+}; // Функция возврата
+
+
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+  element.classList.remove(this.daClassname);
+
+  if (parent.children[index] !== undefined) {
+    parent.children[index].insertAdjacentElement('beforebegin', element);
+  } else {
+    parent.insertAdjacentElement('beforeend', element);
+  }
+}; // Функция получения индекса внутри родителя
+
+
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+  var array = Array.prototype.slice.call(parent.children);
+  return Array.prototype.indexOf.call(array, element);
+}; // Функция сортировки массива по breakpoint и place
+// по возрастанию для this.type = min
+// по убыванию для this.type = max
+
+
+DynamicAdapt.prototype.arraySort = function (arr) {
+  if (this.type === "min") {
+    Array.prototype.sort.call(arr, function (a, b) {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.place === b.place) {
+          return 0;
+        }
+
+        if (a.place === "first" || b.place === "last") {
+          return -1;
+        }
+
+        if (a.place === "last" || b.place === "first") {
+          return 1;
+        }
+
+        return a.place - b.place;
+      }
+
+      return a.breakpoint - b.breakpoint;
+    });
+  } else {
+    Array.prototype.sort.call(arr, function (a, b) {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.place === b.place) {
+          return 0;
+        }
+
+        if (a.place === "first" || b.place === "last") {
+          return 1;
+        }
+
+        if (a.place === "last" || b.place === "first") {
+          return -1;
+        }
+
+        return b.place - a.place;
+      }
+
+      return b.breakpoint - a.breakpoint;
+    });
+    return;
+  }
+};
+
+var da = new DynamicAdapt("max");
+da.init();
+var iconMenu = document.querySelector(".icon-menu");
+var header = document.querySelector('.header');
+
+if (iconMenu) {
+  var menuContent = document.querySelector(".menu");
+  iconMenu.addEventListener('click', function () {
+    document.body.classList.toggle("lock");
+    iconMenu.classList.toggle("active");
+    header.classList.toggle('active');
+    menuContent.classList.toggle("active");
+  });
 }
